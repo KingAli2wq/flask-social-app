7b6a3143dd6cd0a3a28e256337cb3af155238777
KingAli2w <alighanberi9@gmail.com>
2025-11-28 18:01:18 +0000

    Fix Alembic merge graph and clean invalid merge migrations

--- alembic/versions/1fe29d00399b_merge_new_engagement_branch.py
+++ alembic/versions/1fe29d00399b_merge_new_engagement_branch.py
@@ -1,19 +1,23 @@
 """Merge new engagement branch
 
 Revision ID: 1fe29d00399b
-Revises: 08bd1470d6b3, 20241209_merge_notification_and_engagement_heads
+Revises: 7f1d8d2c9c01, 20241208_add_post_engagement_and_message_replies
 Create Date: 2025-11-28 17:35:34.026394
-
 """
 from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
-revision: str = '1fe29d00399b'
-down_revision: Union[str, Sequence[str], None] = ('08bd1470d6b3', '20241209_merge_notification_and_engagement_heads')
+revision: str = "1fe29d00399b"
+
+# The REAL two heads before the duplicate merge files were created
+down_revision: Union[str, Sequence[str], None] = (
+    "7f1d8d2c9c01",
+    "20241208_add_post_engagement_and_message_replies",
+)
+
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -26,3 +30,4 @@ def upgrade() -> None:
 def downgrade() -> None:
     """Downgrade schema."""
     pass
+
